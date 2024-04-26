9601be083ea673e74036126dbba3f0f2f0103b6d
antonyraja1985 <antonyrajamca@gmail.com>
2024-04-26 10:18:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to ReactJs</h1>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
